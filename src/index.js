3f79685080e028cb7705ab6c8604083d7c7eb741
KirillFilipp <kirill_filipp@mail.ru>
2018-10-06 21:10:01 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,39 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = initialValue;
+    // return eval(this.str);
+  }
+  valueOf() {
+    return eval(this.value);
   }
 
   add(number) {
+    this.value += `+ ${number}`;
+    return this;
     // your implementation
   }
-  
+
   subtract(number) {
+    this.value += `- ${number}`;
+    return this;
     // your implementation
   }
 
   multiply(number) {
+    this.value += `* ${number}`;
+    return this;
     // your implementation
   }
 
   devide(number) {
+    this.value += `/ ${number}`;
+    return this;
     // your implementation
   }
 
   pow(number) {
+    this.value += `** ${number}`;
+    return this;
     // your implementation
   }
 }
